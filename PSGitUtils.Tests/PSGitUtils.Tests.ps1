b7e94d75854f8e2e5a0b80678811964efde397c5
capdiem <capdiem@live.com>
2020-05-21 15:18:37 +0800

    :white_check_mark: test: Add Invoke-Emojify tests

---
+++ PSGitUtils.Tests/PSGitUtils.Tests.ps1
@@ -0,0 +1,30 @@
+Import-Module $PSScriptRoot\..\PSGitUtils\PSGitUtils.psd1 -Force
+
+Describe "PSGitUtils" {
+  $singleCode = ":boom: break changes"
+  $singleCodes = ":boom: break chages", ":bug: fix: something wrong!"
+  $multiCode = ":ok_hand: fix: Use :wastebasket: instead of :fire:"
+
+  Context "Invoke-Emojify" {
+    It "success for single-Gitmoji string" {
+      Invoke-Emojify $singleCode | Should Be '💥 break changes'
+    }
+
+    It "success for single-Gitmoji array" {
+      Invoke-Emojify $singleCodes | Should be "💥 break chages", "🐛 fix: something wrong!"
+    }
+
+    It "success for muilt-Gitmoji string" {
+      Invoke-Emojify $multiCode | Should be "👌 fix: Use 🗑️ instead of 🔥"
+    }
+
+    It "success for no-Gitmoji string" {
+      $str = "feat: Add README.md"
+      Invoke-Emojify $str | Should be $str
+    }
+
+    It "success for alias emojify" {
+      Invoke-Emojify $multiCode | Should be "👌 fix: Use 🗑️ instead of 🔥"
+    }
+  }
+}
